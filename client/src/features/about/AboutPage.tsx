0feb18f1baa332e6cbb72f882e40ee25787887ee
vincent <cinemavin@gmail.com>
2023-07-02 21:10:53 +0800

    End of section 4

---
+++ client/src/features/about/AboutPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const AboutPage = () => {
+    return (
+        <Typography variant="h2">AboutPage</Typography>
+    )
+}
+
+export default AboutPage
---
+++ client/src/features/home/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+const HomePage = () => {
+    return (
+        <Typography variant="h2">HomePage</Typography>
+    )
+}
+
+export default HomePage
